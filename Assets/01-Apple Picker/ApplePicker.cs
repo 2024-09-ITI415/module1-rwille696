714b668ec505ef76012422cc97737543a5dbeead
rwille696 <rw696@scarletmail.rutgers.edu>
2024-10-03 13:53:03 -0400

    Finished Apple Picker

--- Assets/01-Apple Picker/ApplePicker.cs
+++ Assets/01-Apple Picker/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour {
     [Header("Inscribed")]
@@ -8,14 +9,41 @@ public class ApplePicker : MonoBehaviour {
     public int numBaskets =3;
     public float basketBottomY =-14f;
     public float basketSpacingY =2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start() {
+        basketList = new List<GameObject>();
         for(int i=0;i<numBaskets;i++){
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab );
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY *i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
+        }
+    
+            }
+
+public void AppleMissed(){
+            //Destroy all of the falling apples
+
+            GameObject[]appleArray=GameObject.FindGameObjectsWithTag("Apple");
+            foreach (GameObject tempGo in appleArray) {
+                Destroy(tempGo);
+        }
+
+        //Destroy one of the baskets
+        //get the index of the last basket in basketList
+        int basketIndex = basketList.Count -1;
+        //get reference to that Basket GameObject
+        GameObject basketGO = basketList[basketIndex];
+        //remove basket from the list and destroy gameobject
+        basketList.RemoveAt(basketIndex);
+        Destroy(basketGO);
+
+        //if there are no Baskets left, restart the game
+        if(basketList.Count == 0) {
+            SceneManager.LoadScene("_Scene_0");
         }
     }  
         
