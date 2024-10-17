0649a4eabac171145e65c9df5e670512bd68e902
rwille696 <rw696@scarletmail.rutgers.edu>
2024-10-16 23:07:20 -0400

    Can't get score to display. I tried to add debug into code but that did not help.

--- Assets/03-Prototype1/Scripts/ScoreDisplay.cs
+++ Assets/03-Prototype1/Scripts/ScoreDisplay.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI; 
+using TMPro;
 
 public class ScoreDisplay : MonoBehaviour
 {
-    public Text scoreText;
+   // public Text scoreText;
+
+    public TMP_Text scoreText;
     public Bunny_Collision bunnyCollision;
 
     void Start()
@@ -16,7 +19,20 @@ public class ScoreDisplay : MonoBehaviour
     {
         // Update the score text based on the current score
         scoreText.text = "Score: " + bunnyCollision.score.ToString();
+//delete
+        if (bunnyCollision != null && scoreText != null)
+    {
+        scoreText.text = "Score: " + bunnyCollision.score.ToString();
+        Debug.Log("Score updated: " + bunnyCollision.score); // Check if the score updates
+    }
+    else
+    {
+        Debug.LogError("Cannot update score display: Either bunnyCollision or scoreText is null.");
+    } //delete
     }
 
     // Call this function whenever the score changes, instead of every frame
+
+// delete below if problem
+    
 }
